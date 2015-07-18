fac88ad372a29178a6f0feeda9789abe2a9c3f7f
radekg <radek@gruchalski.com>
2015-07-18 22:04:39 +0200

    Redis 3.0.3

--- build-redis/build-redis.sh
+++ build-redis/build-redis.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-REDIS_VERSION=3.0.2
-mkdir -p build
-cd build
+REDIS_VERSION=3.0.3
+mkdir -p /tmp/build
+cd /tmp/build
 wget http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz
 tar -xvzf redis-${REDIS_VERSION}.tar.gz
 cd redis-${REDIS_VERSION}
 make
+make test || exit
+
+cp /tmp/build/src/redis-server /vagrant/redis-server-${REDIS_VERSION}
 
 echo " \o/ Redis build should now be available in src/"
